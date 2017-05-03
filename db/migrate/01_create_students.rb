b4e198d334001c87ea6f1f500740ab6c2eb38881
sochetanie <allagorik2901@gmail.com>
2017-05-03 17:59:14 -0400

    doone

--- db/migrate/01_create_students.rb
+++ db/migrate/01_create_students.rb
@@ -1,2 +1,11 @@
 class CreateStudents < ActiveRecord::Migration
+
+  def change
+    create_table :students do |t|
+      t.string :name
+      t.string :genre
+      t.integer :age
+    end
+  end
+
 end
